db91d08b5798439b94a0f9d8a33c52c9707affd9
Paavo Penttila <thxmuffe@hotmail.com>
2021-09-26 19:51:18 +0300

    experimental..

--- account_manager.py
+++ account_manager.py
@@ -1,14 +1,35 @@
+import json
+
+accounts_file = 'accounts.json' # path to settings
+
 class AccountManager:
 
     def __init__(self):
-        self.pincode = 1234
-        self.balance = 500 # this is in EUR.
+        with open(accounts_file) as json_file:
+            self.data = json.load(json_file)
 
     def findWithPinCode(self, code):
-        return code == self.pincode
+        for p in data['accounts']:
+            if p['pincode'] == code:
+                return True
+        
+        print("WARNING: Cannot find account with pin...")
+        return False
 
     def fetchBalance(self):
         return self.balance
 
     def withdraw(self, amount):
         self.balance -= amount
+
+    def saveChanges():
+        data = {}
+        data['accounts'] = []
+        data['accounts'].append({
+            'id': self.accountId,
+            'balance': self.balance,
+            'pincode': self.pincode
+        })
+
+        with open(accounts_file, 'w') as outfile:
+            json.dump(data, outfile)
